f54c3a10c79fee47ee211f2cc3bb359053971e00
marcus plech <mplechdev@gmail.com>
2021-10-30 17:48:16 -0300

    instalando tsx e passando o footer e o header

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./Footer.css";
 import { withRouter } from "react-router";
 
--- src/state/reducers/fetchReducer.js
+++ src/state/reducers/fetchReducer.js
@@ -5,7 +5,6 @@ import {
     FETCH_GENRES,
     FETCH_COMING,
     FETCH_POPULARITY,
-    FETCH_ANIME,
 } from "../action-creators/types";
 
 const INITIAL_STATE = {
